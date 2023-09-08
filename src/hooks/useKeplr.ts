2ed4bd62fa1580aa61d67bcca6fc353f29d2ba5a
2paperstar <lee@paperst.ar>
2023-09-09 01:57:22 +0900

    refactor: change useKeplr type

--- src/hooks/useKeplr.ts
+++ src/hooks/useKeplr.ts
@@ -1,27 +1,25 @@
-import { Keplr } from '@keplr-wallet/types';
+import { Keplr, Window as KeplrWindow } from '@keplr-wallet/types';
 import { useEffect, useState } from 'react';
 
 declare global {
-  interface Window {
-    keplr: Keplr;
-  }
+  interface Window extends KeplrWindow {}
 }
 
 const useKeplr = () => {
-  const [keplr, setKeplr] = useState<Keplr>();
+  const [keplr, setKeplr] = useState<Keplr | null>();
   useEffect(() => {
     if (window.keplr) {
       setKeplr(window.keplr);
     }
 
     if (document.readyState === 'complete') {
-      setKeplr(window.keplr);
+      setKeplr(window.keplr ?? null);
     }
 
     const stateChangeHandler = (event: Event) => {
       if ((event.target as Document | undefined)?.readyState !== 'complete')
         return;
-      setKeplr(window.keplr);
+      setKeplr(window.keplr ?? null);
       document.removeEventListener('readystatechange', stateChangeHandler);
     };
     document.addEventListener('readystatechange', stateChangeHandler);

0affa5b91a38fd59bb880cc78d88ad74d21ee244
2paperstar <lee@paperst.ar>
2023-09-09 23:39:40 +0900

    feat: join design

--- src/pages/Join/index.tsx
+++ src/pages/Join/index.tsx
@@ -1,4 +1,12 @@
-import { Button, Center, Flex, Select, Text, TextInput } from '@mantine/core';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Select,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import useJoin from './useJoin';
 
 const JoinPage = () => {
@@ -18,42 +26,85 @@ const JoinPage = () => {
   return (
     <Flex h="100%" direction="column" justify="center">
       <form onSubmit={handleJoin}>
-        <Flex direction="column" gap="1.5rem" align="center">
+        <Flex direction="column" gap="1em" align="center">
           <TextInput
-            label="Enter Opponent Name"
+            label="Opponent Name"
             placeholder="Bob"
             name="opponent"
             w="100%"
             withAsterisk
             required
-            labelProps={{ mb: '0.5em' }}
+            labelProps={{ mb: '0.5em', px: '0.5em' }}
+            wrapperProps={{
+              sx: {
+                '& input': {
+                  height: '1.5em',
+                  borderRadius: '1em',
+                  paddingInline: '1em',
+                  fontWeight: '600',
+                },
+              },
+            }}
           />
           <TextInput
-            label="Enter Opponent Address"
+            label="Opponent Address"
             placeholder="celestia1234567890abcdef"
             name="address"
             w="100%"
             withAsterisk
             required
-            labelProps={{ mb: '0.5em' }}
+            labelProps={{ mb: '0.5em', px: '0.5em' }}
+            wrapperProps={{
+              sx: {
+                '& input': {
+                  height: '1.5em',
+                  borderRadius: '1em',
+                  paddingInline: '1em',
+                  fontWeight: '600',
+                },
+              },
+            }}
           />
           <Select
-            label="Select Relying Server"
-            placeholder="Select a server"
+            label="Relying Server"
             w="100%"
             data={relyingServers}
             name="server"
             withAsterisk
-            labelProps={{ mb: '0.5em' }}
+            labelProps={{ mb: '0.5em', px: '0.5em' }}
+            maxDropdownHeight={400}
+            wrapperProps={{
+              sx: {
+                '& input': {
+                  height: '1.5em',
+                  borderRadius: '1em',
+                  paddingInline: '1em',
+                  fontWeight: '600',
+                },
+              },
+            }}
           />
+          <Box style={{ flexBasis: 20 }} />
           <Flex gap="1rem">
-            <Button onClick={copyAddress}>Copy my address</Button>
-            <Button type="submit" loading={joining}>
+            <Button
+              onClick={copyAddress}
+              variant="gradient"
+              gradient={{ from: '#87BCF7', to: '#C07FF7', deg: 130 }}
+            >
+              Copy my address
+            </Button>
+            <Button
+              type="submit"
+              loading={joining}
+              variant="gradient"
+              gradient={{ from: '#87BCF7', to: '#C07FF7', deg: 130 }}
+            >
               <Text>Join</Text>
             </Button>
           </Flex>
         </Flex>
       </form>
+      <Box style={{ flexBasis: 100 }} />
     </Flex>
   );
 };

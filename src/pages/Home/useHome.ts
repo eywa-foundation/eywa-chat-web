5ec91534e7e17fe8b4627799afb0ffc18a53675d
2paperstar <lee@paperst.ar>
2023-09-10 02:19:33 +0900

    feat: generate public key

--- src/pages/Home/useHome.ts
+++ src/pages/Home/useHome.ts
@@ -1,20 +1,27 @@
 import { useNavigate } from 'react-router';
 import useKeplr from '../../hooks/useKeplr';
 import { useEffect, useState } from 'react';
+import useKeyPairStore from '../../hooks/useKeyPairStore';
 
 const useHome = () => {
   const navigate = useNavigate();
   const [ready, setReady] = useState(false);
   const { keplr } = useKeplr({ enabled: ready });
+  const { publicKey, generateKeyPair } = useKeyPairStore();
 
   const start = () => {
     setReady(true);
   };
 
   useEffect(() => {
-    if (!keplr) return;
+    if (publicKey) return;
+    generateKeyPair();
+  }, [generateKeyPair, publicKey]);
+
+  useEffect(() => {
+    if (!keplr || !publicKey) return;
     navigate('/list');
-  }, [keplr, navigate, ready]);
+  }, [keplr, navigate, publicKey]);
 
   return { start };
 };

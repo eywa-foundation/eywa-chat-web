5756b020170e7f3938ffee1e2005b9876b50e483
2paperstar <lee@paperst.ar>
2023-09-09 23:44:22 +0900

    feat: change logo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,9 +8,18 @@ const Logo = () => (
     bg="linear-gradient(130deg, #87BCF7 0%, #C07FF7 100%)"
     style={{ borderRadius: '1rem' }}
   >
-    <Text size="6rem" color="white">
-      E
-    </Text>
+    <svg
+      width="4em"
+      height="6em"
+      viewBox="0 0 6 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 10C0.447715 10 0 9.55228 0 9V1.5C0 0.947715 0.447715 0.5 1 0.5H5.25C5.66421 0.5 6 0.835786 6 1.25V1.25C6 1.66421 5.66421 2 5.25 2H2.5C1.94772 2 1.5 2.44772 1.5 3V3.5C1.5 4.05228 1.94772 4.5 2.5 4.5H5.25C5.66421 4.5 6 4.83579 6 5.25V5.25C6 5.66421 5.66421 6 5.25 6H2.5C1.94772 6 1.5 6.44772 1.5 7V7.5C1.5 8.05228 1.94772 8.5 2.5 8.5H5.25C5.66421 8.5 6 8.83579 6 9.25V9.25C6 9.66421 5.66421 10 5.25 10H1Z"
+        fill="white"
+      />
+    </svg>
   </Center>
 );
 
@@ -24,12 +33,13 @@ const HomePage = () => {
           <Text size="2rem">Eywa</Text>
         </Flex>
       </Flex>
-      <Flex direction="column" my="2rem" gap="1rem">
-        <Button onClick={start}>
+      <Flex direction="column" my="4rem" gap="1rem">
+        <Button
+          onClick={start}
+          variant="gradient"
+          gradient={{ from: '#87BCF7', to: '#C07FF7', deg: 130 }}
+        >
           <Text>Starts with Wallet</Text>
-        </Button>
-        <Button variant="outline">
-          <Text>What is Eywa?</Text>
         </Button>
       </Flex>
     </Flex>

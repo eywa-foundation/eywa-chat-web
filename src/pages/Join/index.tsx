43e3a5ee9799bb73d62d970d283fb8f4860dba6c
2paperstar <lee@paperst.ar>
2023-09-09 21:03:19 +0900

    feat: copy my address, opponent name

--- src/pages/Join/index.tsx
+++ src/pages/Join/index.tsx
@@ -2,11 +2,16 @@ import { Button, Center, Flex, Text, TextInput } from '@mantine/core';
 import { useNavigate } from 'react-router';
 import useKeplr from '../../hooks/useKeplr';
 import useIgnite from '../../hooks/useIgnite';
+import { useClipboard } from '@mantine/hooks';
 
 const JoinPage = () => {
   const { accounts, error, loading } = useKeplr();
   const navigate = useNavigate();
   useIgnite((!error && accounts?.length && !loading) || false);
+  const { copy } = useClipboard();
+  const copyAddress = () => {
+    copy(accounts?.[0].address ?? '');
+  };
 
   const handleJoin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -26,22 +31,32 @@ const JoinPage = () => {
   if (loading) return null;
 
   return (
-    <Center mih="100vh">
+    <Flex h="100%" direction="column" justify="center">
       <form onSubmit={handleJoin}>
         <Flex direction="column" gap="1rem" align="center">
-          <Text>my address: {accounts[0].address}</Text>
           <TextInput
-            label="Enter Bob address"
-            placeholder="cosmos1234567890abcdef"
-            style={{ width: '400px' }}
-            name="address"
+            label="Enter Opponent Name"
+            placeholder="Bob"
+            name="opponent"
+            w="100%"
+            withAsterisk
           />
-          <Button type="submit">
-            <Text>Join</Text>
-          </Button>
+          <TextInput
+            label="Enter Opponent Address"
+            placeholder="celestia1234567890abcdef"
+            name="address"
+            w="100%"
+            withAsterisk
+          />
+          <Flex gap="1rem">
+            <Button onClick={copyAddress}>Copy my address</Button>
+            <Button type="submit">
+              <Text>Join</Text>
+            </Button>
+          </Flex>
         </Flex>
       </form>
-    </Center>
+    </Flex>
   );
 };
 
